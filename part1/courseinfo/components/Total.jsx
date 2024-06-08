bbb9d73198c6fe79ecddad13d646bb9c38faac79
Lucas Passini <47937044+lucaspassini@users.noreply.github.com>
2024-06-08 19:05:59 -0300

    add: complete exercise 1

---
+++ part1/courseinfo/components/Total.jsx
@@ -0,0 +1,12 @@
+const Total = ({ course }) => {
+  let total =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  );
+};
+export default Total;
